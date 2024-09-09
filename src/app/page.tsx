386c03ceeea0cf32b19c142995bd4c51f0351a21
Jean-Philipp Bruehwiler <jpbruehwiler@gmail.com>
2024-09-09 18:55:58 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-blue-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-blue-600">
+        Click Me
+      </Button>
+    </div>
+  );
+}
