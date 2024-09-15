b5cfb82f1f3a12aa12d37f20b20932303d5768c4
Jean-Philipp Bruehwiler <jpbruehwiler@gmail.com>
2024-09-15 12:02:04 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,33 @@
 import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
+import { auth } from "@/auth";
+import Profile from "@/components/profile";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <div>
-      <Button className="bg-blue-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-blue-600">
-        Click Me
-      </Button>
+      <form action={actions.signIn}>
+        <Button
+          type="submit"
+          className="bg-blue-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-blue-600"
+        >
+          Sign In
+        </Button>
+      </form>
+      <form action={actions.signOut}>
+        <Button
+          type="submit"
+          className="bg-blue-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-blue-600"
+        >
+          Sign Out
+        </Button>
+      </form>
+
+      {/* display if user is signed in or not */}
+      {session?.user ? <div>Signed In</div> : <div> Signed Out</div>}
+
+      <Profile />
     </div>
   );
 }
